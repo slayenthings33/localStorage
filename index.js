c446450042443679f66604daac62a0901ec7658d
Ryan Slayen <ryanslayen@gmail.com>
2020-09-01 16:07:18 +0200

    pre parse

--- index.js
+++ index.js
@@ -2,12 +2,15 @@
 
 let uploadButton = document.getElementById("uploadButton");
 let clearButton = document.getElementById("clearButton");
+// let removeButton = document.getElementById("removeButton");
+// let removeInput = document.getElementById("removeInput");
 let uploadSection = document.getElementById("uploadSection");
 
 /*************EVENT LISTENERS**************/
 
 // uploadButton.addEventListener("click", upload);
 // uploadButton.addEventListener("click", displayInput);
+removeButton.addEventListener("click", removeItem);
 clearButton.addEventListener("click", clearLocal)
 uploadButton.addEventListener("click", () => {
     upload();     
@@ -18,35 +21,38 @@ uploadButton.addEventListener("click", () => {
 
 /*************FUNCTIONS**************/
 
+function removeItem() {
+    let toDelete = document.getElementById("removeInput").value;
+    localStorage.removeItem(toDelete);
+    // console.log("function: removeItem");
+}
 
- function upload() {
-     console.log("function: upload")
-     let fullNameInput = document.getElementById("fullName").value;
-     let emailInput = document.getElementById("email").value;
-     let phoneInput = document.getElementById("phoneNumber").value;
-     console.log(phoneInput);
-     localStorage.setItem("fullName", fullNameInput);
-     localStorage.setItem("email", emailInput);
-     localStorage.setItem("phone", phoneInput);    
-    };
-    
-    function displayInput() {
-        let fullNameInput = localStorage.getItem("fullName");
-        let emailInput = localStorage.getItem("email");
-        let phoneInput  = localStorage.getItem("phone");
-        let info = `
-        <li>${fullNameInput}</li>
-        <li>${emailInput}</li>
-        <li>${phoneInput}</li>
-        `;
-        uploadSection.innerHTML += info;
-        console.log("function: displayInput")
-    };
-    
+function upload() {
+    console.log("function: upload")
+    let fullNameInput = document.getElementById("fullName").value;
+    let emailInput = document.getElementById("email").value;
+    let phoneInput = document.getElementById("phoneNumber").value;
+
+    localStorage.setItem("fullName", fullNameInput);
+    localStorage.setItem("email", emailInput);
+    localStorage.setItem("phone", phoneInput);    
+};
+
+function displayInput() {
+    let fullName = localStorage.getItem("fullName");
+    let email = localStorage.getItem("email");
+    let phoneNumber  = localStorage.getItem("phone");
+    let info = `
+    <li>${fullName}</li>
+    <li>${email}</li>
+    <li>${phoneNumber}</li>
+    `;
+    uploadSection.innerHTML += info;
+    // console.log("function: displayInput")
+};
 
     function clearLocal() {
     localStorage.clear();
-    info;
-    uploadSection.innerHTML += info;
-    console.log("function: clearLocal");
+    uploadSection.innerHTML = "";
+    // console.log("function: clearLocal");
     };
